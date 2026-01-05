3a2d04a9ab2ffabed7244d6a436edf2698992067
Guilherme Silva <239806034+dataonegs@users.noreply.github.com>
2026-01-05 13:27:08 -0300

    13:27 -  1 file changed, 1 insertion(+), 1 deletion(-)
    M  git-pipeline.sh

--- git-pipeline.sh
+++ git-pipeline.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -sL https://raw.githubusercontent.com/script-cloud/script-cloud/main/git/git-pipeline-auto.sh | bash
+curl -sL https://raw.githubusercontent.com/script-cloud/script-cloud/main/scripts/git/git-pipeline.sh | bash
